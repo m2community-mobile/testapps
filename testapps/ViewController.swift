0622c327c93d6f4bfbc5a9bc002ca872ca22862f
M2 <m2community.mobile@gmail.com>
2024-09-06 12:32:14 +0900

    Initial Commit

---
+++ testapps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testapps
+//
+//  Created by m2comm on 2024/09/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
